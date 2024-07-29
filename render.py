02096502b12b73296bab382dc9e1cc9c2deef23e
GuoJunfu-tech <guojunfu@mail.ustc.edu.cn>
2024-07-29 21:17:23 +0800

    first version, need to upgrade further

--- render.py
+++ render.py
@@ -1,66 +1,264 @@
-#
-# Copyright (C) 2023, Inria
-# GRAPHDECO research group, https://team.inria.fr/graphdeco
-# All rights reserved.
-#
-# This software is free for non-commercial, research and evaluation use 
-# under the terms of the LICENSE.md file.
-#
-# For inquiries contact  george.drettakis@inria.fr
-#
-
-import torch
-from scene import Scene
 import os
-from tqdm import tqdm
+from typing import NamedTuple
 from os import makedirs
-from gaussian_renderer import render
-import torchvision
-from utils.general_utils import safe_state
 from argparse import ArgumentParser
-from arguments import ModelParams, PipelineParams, get_combined_args
+from typing import List
+import torch
+import torchvision
+import json
+import numpy as np
+from tqdm import tqdm
+from scene import Scene
+from utils.general_utils import safe_state
 from gaussian_renderer import GaussianModel
+from utils.graphics_utils import focal2fov, fov2focal
+from diff_gaussian_rasterization import (
+    GaussianRasterizationSettings,
+    GaussianRasterizer,
+)
 
-def render_set(model_path, name, iteration, views, gaussians, pipeline, background):
-    render_path = os.path.join(model_path, name, "ours_{}".format(iteration), "renders")
-    gts_path = os.path.join(model_path, name, "ours_{}".format(iteration), "gt")
 
-    makedirs(render_path, exist_ok=True)
-    makedirs(gts_path, exist_ok=True)
+# this file is used to render imgs from given sequence of extrinsic and intrinsic
 
-    for idx, view in enumerate(tqdm(views, desc="Rendering progress")):
-        rendering = render(view, gaussians, pipeline, background)["render"]
-        gt = view.original_image[0:3, :, :]
-        torchvision.utils.save_image(rendering, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
-        torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
 
-def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParams, skip_train : bool, skip_test : bool):
+class CameraInfo(NamedTuple):
+    uid: int
+    R: np.array
+    T: np.array
+    FovY: np.array
+    FovX: np.array
+    # image: np.array
+    # width: int
+    # height: int
+    # image_path: str
+    image_name: str
+
+
+def construct_camera_info_from_transforms(
+    transforms_json_file: str,
+    image_size: list = [800, 800],
+) -> List[CameraInfo]:
+    """a simple version of the function readCamerasFromTransforms
+
+    Args:
+        path (str):
+        transformfile (str):
+
+    Returns:
+        CameraInfo:
+    """
+    cam_infos = []
+
+    with open(transforms_json_file) as json_file:
+        contents = json.load(json_file)
+        fovx = contents["camera_angle_x"]
+
+        frames = contents["frames"]
+        for idx, frame in enumerate(frames):
+            # cam_name = os.path.join(path, frame["file_path"])
+
+            # NeRF 'transform_matrix' is a camera-to-world transform
+            c2w = np.array(frame["transform_matrix"])
+            # change from OpenGL/Blender camera axes (Y up, Z back) to COLMAP (Y down, Z forward)
+            c2w[:3, 1:3] *= -1
+
+            # get the world-to-camera transform and set R, T
+            w2c = np.linalg.inv(c2w)
+            R = np.transpose(
+                w2c[:3, :3]
+            )  # R is stored transposed due to 'glm' in CUDA code
+            T = w2c[:3, 3]
+
+            fovy = focal2fov(fov2focal(fovx, image_size[0]), image_size[1])
+            FovY = fovy
+            FovX = fovx
+
+            cam_infos.append(
+                CameraInfo(
+                    uid=idx,
+                    R=R,
+                    T=T,
+                    FovY=FovY,
+                    FovX=FovX,
+                    width=image_size[0],
+                    height=image_size[1],
+                )
+            )
+
+    return cam_infos
+
+
+def render(
+    viewpoint_camera,
+    pc: GaussianModel,
+    pipe,
+    bg_color: torch.Tensor,
+    scaling_modifier=1.0,
+    override_color=None,
+):
+    """
+    This code is modified from 3DGS, but way more simpler.
+
+    Render the scene.
+    Background tensor (bg_color) must be on GPU!
+    """
+
+    # Create zero tensor. We will use it to make pytorch return gradients of the 2D (screen-space) means
+    screenspace_points = (
+        torch.zeros_like(
+            pc.get_xyz, dtype=pc.get_xyz.dtype, requires_grad=True, device="cuda"
+        )
+        + 0
+    )
+    try:
+        screenspace_points.retain_grad()
+    except:
+        pass
+
+    # Set up rasterization configuration
+    tanfovx = math.tan(viewpoint_camera.FoVx * 0.5)
+    tanfovy = math.tan(viewpoint_camera.FoVy * 0.5)
+
+    raster_settings = GaussianRasterizationSettings(
+        image_height=int(viewpoint_camera.image_height),
+        image_width=int(viewpoint_camera.image_width),
+        tanfovx=tanfovx,
+        tanfovy=tanfovy,
+        bg=bg_color,
+        scale_modifier=scaling_modifier,
+        viewmatrix=viewpoint_camera.world_view_transform,
+        projmatrix=viewpoint_camera.full_proj_transform,
+        sh_degree=pc.active_sh_degree,
+        campos=viewpoint_camera.camera_center,
+        prefiltered=False,
+        debug=pipe.debug,
+    )
+
+    rasterizer = GaussianRasterizer(raster_settings=raster_settings)
+
+    means3D = pc.get_xyz
+    means2D = screenspace_points
+    opacity = pc.get_opacity
+
+    # If precomputed 3d covariance is provided, use it. If not, then it will be computed from
+    # scaling / rotation by the rasterizer.
+    scales = None
+    rotations = None
+    cov3D_precomp = None
+    if pipe.compute_cov3D_python:
+        cov3D_precomp = pc.get_covariance(scaling_modifier)
+    else:
+        scales = pc.get_scaling
+        rotations = pc.get_rotation
+
+    # If precomputed colors are provided, use them. Otherwise, if it is desired to precompute colors
+    # from SHs in Python, do it. If not, then SH -> RGB conversion will be done by rasterizer.
+    shs = None
+    colors_precomp = None
+    if override_color is None:
+        if pipe.convert_SHs_python:
+            shs_view = pc.get_features.transpose(1, 2).view(
+                -1, 3, (pc.max_sh_degree + 1) ** 2
+            )
+            dir_pp = pc.get_xyz - viewpoint_camera.camera_center.repeat(
+                pc.get_features.shape[0], 1
+            )
+            dir_pp_normalized = dir_pp / dir_pp.norm(dim=1, keepdim=True)
+            sh2rgb = eval_sh(pc.active_sh_degree, shs_view, dir_pp_normalized)
+            colors_precomp = torch.clamp_min(sh2rgb + 0.5, 0.0)
+        else:
+            shs = pc.get_features
+    else:
+        colors_precomp = override_color
+
+    # Rasterize visible Gaussians to image, obtain their radii (on screen).
+    rendered_image, radii = rasterizer(
+        means3D=means3D,
+        means2D=means2D,
+        shs=shs,
+        colors_precomp=colors_precomp,
+        opacities=opacity,
+        scales=scales,
+        rotations=rotations,
+        cov3D_precomp=cov3D_precomp,
+    )
+
+    # Those Gaussians that were frustum culled or had a radius of 0 were not visible.
+    # They will be excluded from value updates used in the splitting criteria.
+    return {
+        "render": rendered_image,
+        "viewspace_points": screenspace_points,
+        "visibility_filter": radii > 0,
+        "radii": radii,
+    }
+
+
+def render_from_views(
+    # pipe: PipelineParams,
+    gs_path: str,
+    view_path: str,
+    output_path: str,
+) -> None:
+    if not os.path.exists(view_path):
+        raise ValueError(f"path {view_path} does not exist")
+
+    class PIPE:
+        def __init__(self):
+            self.convert_SHs_python = False
+            self.compute_cov3D_python = False
+            self.debug = False
+
+    pipe = PIPE()
+
+    train_cam_infos = construct_camera_info_from_transforms(view_path)
+
+    print("Reading Test Transforms")
     with torch.no_grad():
-        gaussians = GaussianModel(dataset.sh_degree)
-        scene = Scene(dataset, gaussians, load_iteration=iteration, shuffle=False)
+        gaussians = GaussianModel(sh_degree=3)
+        gaussians.load_ply(gs_path)
+        print(f"gs model loaded from {gs_path}")
 
-        bg_color = [1,1,1] if dataset.white_background else [0, 0, 0]
+        # scene = Scene(dataset, gaussians, shuffle=False)
+
+        bg_color = [0, 0, 0]
         background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
 
-        if not skip_train:
-             render_set(dataset.model_path, "train", scene.loaded_iter, scene.getTrainCameras(), gaussians, pipeline, background)
+        for id, view in enumerate(train_cam_infos):
+            render_img = render(view, gaussians, pipe, background)["render"]
 
-        if not skip_test:
-             render_set(dataset.model_path, "test", scene.loaded_iter, scene.getTestCameras(), gaussians, pipeline, background)
+            torchvision.utils.save_image(
+                render_img, os.path.join(output_path, "{0:05d}".format(id) + ".png")
+            )
+
 
 if __name__ == "__main__":
     # Set up command line argument parser
-    parser = ArgumentParser(description="Testing script parameters")
-    model = ModelParams(parser, sentinel=True)
-    pipeline = PipelineParams(parser)
-    parser.add_argument("--iteration", default=-1, type=int)
-    parser.add_argument("--skip_train", action="store_true")
-    parser.add_argument("--skip_test", action="store_true")
-    parser.add_argument("--quiet", action="store_true")
-    args = get_combined_args(parser)
-    print("Rendering " + args.model_path)
+    # PARSER = ArgumentParser(description="Testing script parameters")
+    # # MODEL = ModelParams(PARSER, sentinel=True)
+    # PIPELINE = PipelineParams(PARSER)
+
+    # PARSER.add_argument("--gs_path", type=str)
+    # PARSER.add_argument("--view_path", type=str)
+
+    ROOT = os.getcwd()
+    output_path = os.path.join(ROOT, "output")
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+
+    # PARSER.add_argument("--iteration", default=-1, type=int)
+    # PARSER.add_argument("--skip_train", action="store_true")
+    # PARSER.add_argument("--skip_test", action="store_true")
+    # PARSER.add_argument("--quiet", action="store_true")
+    # args = get_combined_args(PARSER)
 
     # Initialize system state (RNG)
-    safe_state(args.quiet)
+    # safe_state(args.quiet)
+    gs_path = os.path.join(ROOT, "closet-end-m.ply")
+    view_path = os.path.join(ROOT, "transforms_train.json")
 
-    render_sets(model.extract(args), args.iteration, pipeline.extract(args), args.skip_train, args.skip_test)
+    render_from_views(
+        # PIPELINE.extract(args),
+        gs_path,
+        view_path,
+        output_path,
+    )
